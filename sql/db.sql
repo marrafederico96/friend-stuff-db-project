b0c20d6904c0e6654ee0ed2b55b03f73a94352ab
Federico Marra <marrafederico@proton.me>
2025-06-16 13:29:07 +0200

    update db.sql

--- sql/db.sql
+++ sql/db.sql
@@ -1,118 +1,118 @@
 CREATE DATABASE IF NOT EXISTS `friend_stuff`;
 
-USE DATABASE `friend_stuff`;
+USE `friend_stuff`;
 
 CREATE TABLE IF NOT EXISTS `users` (
-    `user_id` AUTO_INCREMENT NOT NULL INT,
-    `username` NOT NULL UNIQUE VARCHAR(100),
-    `email` NOT NULL UNIQUE VARCHAR(100),
-    `first_name` NOT NULL VARCHAR(100),
-    `last_name` NOT NULL VARCHAR(100),
+    `user_id` INT AUTO_INCREMENT NOT NULL,
+    `username` VARCHAR(100) NOT NULL UNIQUE,
+    `email` VARCHAR(100) NOT NULL UNIQUE,
+    `first_name` VARCHAR(100) NOT NULL,
+    `last_name` VARCHAR(100) NOT NULL,
     PRIMARY KEY (`user_id`)
 );
 
 CREATE TABLE IF NOT EXISTS `friendships` (
-    `user1_id` NOT NULL INT,
-    `user2_id` NOT NULL INT,
-    `state_request` NOT NULL ENUM ('pending', 'accepted', 'denied') DEFAULT 'pending',
+    `user1_id` INT NOT NULL,
+    `user2_id` INT NOT NULL,
+    `state_request` ENUM ('pending', 'accepted', 'denied') DEFAULT 'pending' NOT NULL,
     PRIMARY KEY (`user1_id`, `user2_id`),
     FOREIGN KEY (`user1_id`) REFERENCES users (`user_id`),
     FOREIGN KEY (`user2_id`) REFERENCES users (`user_id`)
 );
 
 CREATE TABLE IF NOT EXISTS `groups` (
-    `group_id` NOT NULL AUTO_INCREMENT INT,
-    `group_name` NOT NULL VARCHAR(100),
-    `description` NULL VARCHAR(255),
-    `creation_date` NOT NULL DATETIME DEFAULT CURRENT_TIMESTAMP,
+    `group_id` INT NOT NULL AUTO_INCREMENT,
+    `group_name` VARCHAR(100) NOT NULL,
+    `description` VARCHAR(255) NULL,
+    `creation_date` DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
     PRIMARY KEY (`group_id`)
 );
 
 CREATE TABLE IF NOT EXISTS `group_members` (
-    `group_id` NOT NULL INT,
-    `user_id` NOT NULL INT,
-    `join_date` NOT NULL DATETIME DEFAULT CURRENT_TIMESTAMP,
-    `role` NOT NULL ENUM ('admin', 'member') DEFAULT 'member',
+    `group_id` INT NOT NULL,
+    `user_id` INT NOT NULL,
+    `join_date` DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    `role` ENUM ('admin', 'member') DEFAULT 'member' NOT NULL,
     PRIMARY KEY (`group_id`, `user_id`),
     FOREIGN KEY (`group_id`) REFERENCES groups (`group_id`),
     FOREIGN KEY (`user_id`) REFERENCES users (`user_id`)
 );
 
 CREATE TABLE IF NOT EXISTS `locations` (
-    `location_id` NOT NULL AUTO_INCREMENT INT,
-    `location_name` NOT NULL VARCHAR(100),
-    `city` NULL VARCHAR(100),
-    `street` NULL VARCHAR(100),
-    `street_number` NULL INT,
+    `location_id` INT NOT NULL AUTO_INCREMENT,
+    `location_name` VARCHAR(100) NOT NULL,
+    `city` VARCHAR(100) NULL,
+    `street` VARCHAR(100) NULL,
+    `street_number` INT NULL,
     PRIMARY KEY (`location_id`)
 );
 
 CREATE TABLE IF NOT EXISTS `events` (
-    `event_id` NOT NULL AUTO_INCREMENT INT,
-    `group_id` NOT NULL INT,
-    `location_id` NOT NULL INT,
-    `event_title` NOT NULL VARCHAR(100),
-    `category` NOT NULL ENUM (
+    `event_id` INT NOT NULL AUTO_INCREMENT,
+    `group_id` INT NOT NULL,
+    `location_id` INT NOT NULL,
+    `event_title` VARCHAR(100) NOT NULL,
+    `category` ENUM (
         'food',
         'party',
         'holidays',
         'relax',
         'camping',
         'other'
-    ) DEFAULT 'other',
-    `start_date` NOT NULL DATETIME,
-    `end_date` NOT NULL DATETIME,
-    `description` NULL VARCHAR(255),
+    ) DEFAULT 'other' NOT NULL,
+    `start_date` DATETIME NOT NULL,
+    `end_date` DATETIME NOT NULL,
+    `description` VARCHAR(255) NULL,
     PRIMARY KEY (`event_id`),
     FOREIGN KEY (`group_id`) REFERENCES groups (`group_id`),
     FOREIGN KEY (`location_id`) REFERENCES locations (`location_id`)
 );
 
 CREATE TABLE IF NOT EXISTS `chat` (
-    `chat_id` NOT NULL AUTO_INCREMENT INT,
-    `event_id` NOT NULL INT,
+    `chat_id` INT NOT NULL AUTO_INCREMENT,
+    `event_id` INT NOT NULL,
     PRIMARY KEY (`chat_id`),
     FOREIGN KEY (`event_id`) REFERENCES events (`event_id`)
 );
 
 CREATE TABLE IF NOT EXISTS `messages` (
-    `message_id` NOT NULL AUTO_INCREMENT INT,
-    `chat_id` NOT NULL INT,
-    `user_id` NOT NULL INT,
-    `content` NOT NULL TEXT,
-    `send_date` NOT NULL DATETIME DEFAULT CURRENT_TIMESTAMP,
+    `message_id` INT NOT NULL AUTO_INCREMENT,
+    `chat_id` INT NOT NULL,
+    `user_id` INT NOT NULL,
+    `content` TEXT NOT NULL,
+    `send_date` DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
     PRIMARY KEY (`message_id`),
     FOREIGN KEY (`chat_id`) REFERENCES chat (`chat_id`),
     FOREIGN KEY (`user_id`) REFERENCES users (`user_id`)
 );
 
 CREATE TABLE IF NOT EXISTS `attachments` (
-    `attachment_id` NOT NULL AUTO_INCREMENT INT,
-    `message_id` NOT NULL INT,
-    `file_name` NOT NULL VARCHAR(255),
-    `file_type` NOT NULL VARCHAR(255),
-    `file_size` NOT NULL BIGINT,
+    `attachment_id` INT NOT NULL AUTO_INCREMENT,
+    `message_id` INT NOT NULL,
+    `file_name` VARCHAR(255) NOT NULL,
+    `file_type` VARCHAR(255) NOT NULL,
+    `file_size` BIGINT NOT NULL,
     PRIMARY KEY (`attachment_id`),
     FOREIGN KEY (`message_id`) REFERENCES messages (`message_id`)
 );
 
 CREATE TABLE IF NOT EXISTS `expenses` (
-    `expense_id` NOT NULL AUTO_INCREMENT INT,
-    `event_id` NOT NULL INT,
-    `payer_id` NOT NULL INT,
-    `amount` NOT NULL DECIMAL(10, 2),
-    `expense_name` NOT NULL VARCHAR(100),
-    `description` NULL VARCHAR(255),
-    `expense_date` NOT NULL DATETIME DEFAULT CURRENT_TIMESTAMP,
+    `expense_id` INT NOT NULL AUTO_INCREMENT,
+    `event_id` INT NOT NULL,
+    `payer_id` INT NOT NULL,
+    `amount` DECIMAL(10, 2) NOT NULL,
+    `expense_name` VARCHAR(100) NOT NULL,
+    `description` VARCHAR(255) NULL,
+    `expense_date` DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
     PRIMARY KEY (`expense_id`),
     FOREIGN KEY (`event_id`) REFERENCES events (`event_id`),
     FOREIGN KEY (`payer_id`) REFERENCES users (`user_id`)
 );
 
 CREATE TABLE IF NOT EXISTS `expense_contributinos` (
-    `participant_id` NOT NULL INT,
-    `expense_id` NOT NULL INT,
-    `amount_owed` NOT NULL DECIMAL(10, 2),
+    `participant_id` INT NOT NULL,
+    `expense_id` INT NOT NULL,
+    `amount_owed` DECIMAL(10, 2) NOT NULL,
     PRIMARY KEY (`participant_id`, `expense_id`),
     FOREIGN KEY (`participant_id`) REFERENCES users (`user_id`),
     FOREIGN KEY (`expense_id`) REFERENCES expenses (`expense_id`)
